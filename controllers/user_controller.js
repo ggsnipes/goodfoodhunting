d60b1e45d972fa117053c5b84da1ae9f95b4d69c
ggsnipes <31529149+ggsnipes@users.noreply.github.com>
2023-03-13 16:50:34 +1100

    added a sign in page

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const router = express.Router()
+const bcrypt = require("bcrypt")
+const saltRounds = 10
+
+const db = require("./../db")
 
 /*
 
@@ -12,3 +16,43 @@ router.get("/users/:id/edit") // get existing user form
 router.get("/users/:id") // get single user
 
 */
+
+router.get("/users", (req, res) => {
+    res.send("dont press this for now thanks")
+})
+
+
+router.post("/users", (req, res) => {
+    const { email, password } = req.body
+    const sqlCheck = `SELECT * FROM users WHERE email = $1;`
+
+    db.query(sqlCheck, (err, dbRes) => {
+        if(err){
+            const sql = `INSERT INTO users VALUES ('$1, '$2');`
+
+            db.query(sqlSignUp(email, password), (err, dbRes) => {
+                res.redirect("/")
+            })
+        } else {
+            res.redirect("/login")
+        }
+    })
+
+
+})
+
+
+router.get("users/new", (req, res) => {
+    res.render("signup")
+})
+
+
+function sqlSignUp(email, password) {
+    bcrypt.hash(password, saltRounds, (err, hash) => {
+        let hashPw = hash
+    })
+
+    const sql = `INSERT INTO users VALUES ('${email}', '${hashPw}');`
+
+
+}
